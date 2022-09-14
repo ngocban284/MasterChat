eab7f64f11e63394a73c40d20de75e909b16eb36
ngocban <daongocban1102@gmail.com>
2022-09-14 11:25:34 +0700

    delete user api (#3)

--- server/src/api/User/allUsers/allUsers.ts
+++ server/src/api/User/allUsers/allUsers.ts
@@ -6,16 +6,16 @@ const prisma = new PrismaClient();
 
 type result = (User & { rooms: Room[]; messages: Message[] })[];
 
-export const allUsers = async (
-  _: result,
-  __: null,
-  { request, isAuthenticated }: Context,
-): Promise<result> => {
-  const users = await prisma.user.findMany({
-    include: {
-      messages: true,
-      rooms: true,
+export default {
+  Query: {
+    allUsers: (_: result, __: null, { request, isAuthenticated }: Context): Promise<result> => {
+      isAuthenticated(request);
+      return prisma.user.findMany({
+        include: {
+          rooms: true,
+          messages: true,
+        },
+      });
     },
-  });
-  return users;
+  },
 };
--- server/src/api/User/deleteUser/deleteUser.ts
+++ server/src/api/User/deleteUser/deleteUser.ts
@@ -1,3 +1,63 @@
 import { PrismaClient, User, Room, Message } from '@prisma/client';
-
 import { Context } from '@interfaces/context';
+import { withFilter } from 'graphql-subscriptions';
+import TRIGGER from '@utils/trigger';
+
+const prisma = new PrismaClient();
+
+export default {
+  Mutation: {
+    deleteUser: async (
+      _: boolean,
+      __: null,
+      { request, isAuthenticated, pubsub }: Context,
+    ): Promise<boolean> => {
+      isAuthenticated(request);
+      const { id, roomId } = request.user;
+
+      await prisma.user.update({
+        where: {
+          id,
+        },
+        data: {
+          isDeleted: true,
+        },
+      });
+
+      const restUser = await prisma.user.count({
+        where: {
+          isDeleted: false,
+          rooms: {
+            some: {
+              id: roomId,
+            },
+          },
+        },
+      });
+
+      if (restUser === 0) {
+        // Delete room
+        await prisma.$queryRaw`DELETE FROM Room WHERE id = ${roomId}`;
+        // delete all user in room
+        await prisma.$queryRaw`delete from User where User.id in (select B from Room join _RoomToUser on A = ${roomId} AND Room.id = A );`;
+      }
+
+      return true;
+    },
+  },
+
+  Subscription: {
+    deleteUser: {
+      subscribe: withFilter(
+        (_: boolean, __: null, { pubsub }: Context) => pubsub.asyncIterator(TRIGGER.DELETE_USER),
+        async (payload, variables, context): Promise<boolean> => {
+          const { roomId } = context.request.user;
+          if (payload.deleteUser.roomId === roomId) {
+            return true;
+          }
+          return false;
+        },
+      ),
+    },
+  },
+};
