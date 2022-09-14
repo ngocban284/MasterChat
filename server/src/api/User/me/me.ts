2a723a9e7ee21c9a8aee5953adc7dc091da636d8
ngocban <daongocban1102@gmail.com>
2022-09-14 11:32:55 +0700

    me api (#3)

---
+++ server/src/api/User/me/me.ts
@@ -0,0 +1,23 @@
+import { PrismaClient, User, Room, Message } from '@prisma/client';
+
+import { Context } from '@interfaces/context';
+
+const prisma = new PrismaClient();
+
+export default {
+  Query: {
+    me: async (
+      _: User | null,
+      __: null,
+      { request, isAuthenticated }: Context,
+    ): Promise<User | null> => {
+      isAuthenticated(request);
+      const { id } = request.user;
+      return prisma.user.findOne({
+        where: {
+          id: id,
+        },
+      });
+    },
+  },
+};
