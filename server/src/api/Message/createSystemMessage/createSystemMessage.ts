adc2bb08571a12b487e0bb321efce46a0a318ea8
ngocban <daongocban1102@gmail.com>
2022-09-15 22:41:27 +0700

    create system message api (#3)

---
+++ server/src/api/Message/createSystemMessage/createSystemMessage.ts
@@ -0,0 +1,46 @@
+import { Context } from '../../../interfaces/context';
+import { PrismaClient } from '@prisma/client';
+import TRIGGER from '@utils/trigger';
+
+const prisma = new PrismaClient();
+
+export default {
+  Mutation: {
+    createMessage: async (
+      _: boolean,
+      args: { source: string },
+      { request, pubsub }: Context,
+    ): Promise<boolean> => {
+      const { id, nickname, avatar, lang, roomId } = request.user;
+      const { source } = args;
+
+      const message = await prisma.message.create({
+        data: {
+          text: nickname,
+          source,
+          user: {
+            connect: {
+              id,
+            },
+          },
+          room: {
+            connect: {
+              id: roomId,
+            },
+          },
+        },
+        include: {
+          user: true,
+        },
+      });
+
+      pubsub.publish(TRIGGER.NEW_MESSAGE, { newMessage: message });
+      if (source === 'in') {
+        pubsub.publish(TRIGGER.NEW_USER, { newUser: { id, nickname, avatar, lang } });
+        return true;
+      }
+      pubsub.publish(TRIGGER.DELETE_USER, { deleteUser: { id, roomId } });
+      return true;
+    },
+  },
+};
