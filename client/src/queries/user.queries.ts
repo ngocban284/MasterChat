d54cc0db956a208171ad0999ae8774c526611bb8
ngocban <daongocban1102@gmail.com>
2022-09-21 21:25:22 +0700

    user client queries

--- client/src/queries/user.queries.ts
+++ client/src/queries/user.queries.ts
@@ -1,1 +1,26 @@
 import { gql } from '@apollo/client';
+
+export const NEW_USER = gql`
+  subscription newUser {
+    newUser {
+      id
+      nickname
+      avatar
+      lang
+    }
+  }
+`;
+
+export const DELETE_USER = gql`
+  mutation {
+    deleteUser
+  }
+`;
+
+export const SUBCRIBE_DELETE_USER = gql`
+  subscription deleteUser {
+    deleteUser {
+      id
+    }
+  }
+`;
