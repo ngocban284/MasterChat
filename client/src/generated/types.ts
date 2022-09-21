79b1d756b9ada8436cfa51ccaba241baff6266c3
ngocban <daongocban1102@gmail.com>
2022-09-21 20:40:29 +0700

    generate types (#5)

---
+++ client/src/generated/types.ts
@@ -0,0 +1,156 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]: Maybe<T[SubKey]> };
+
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  allMessagesByPage?: Maybe<AllMessages>;
+  allMessagesByTime?: Maybe<Array<Maybe<Message>>>;
+  allRooms?: Maybe<Array<Maybe<Room>>>;
+  roomById?: Maybe<Room>;
+  allUsers?: Maybe<Array<Maybe<User>>>;
+  me: User;
+};
+
+export type QueryAllMessagesByPageArgs = {
+  page: Scalars['Int'];
+};
+
+export type QueryAllMessagesByTimeArgs = {
+  time: Scalars['String'];
+};
+
+export type QueryRoomByIdArgs = {
+  id: Scalars['Int'];
+};
+
+export type AllMessages = {
+  __typename?: 'allMessages';
+  messages?: Maybe<Array<Maybe<Message>>>;
+  nextPage?: Maybe<Scalars['Int']>;
+};
+
+export type Message = {
+  __typename?: 'Message';
+  id: Scalars['Int'];
+  text: Scalars['String'];
+  source: Scalars['String'];
+  room: Room;
+  user: User;
+  createdAt?: Maybe<Scalars['String']>;
+  updatedAt?: Maybe<Scalars['String']>;
+};
+
+export type Room = {
+  __typename?: 'Room';
+  id: Scalars['Int'];
+  avatar: Scalars['String'];
+  code: Scalars['String'];
+  users: Array<User>;
+  messages: Array<Message>;
+  createdAt?: Maybe<Scalars['String']>;
+  updatedAt?: Maybe<Scalars['String']>;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['Int'];
+  nickname: Scalars['String'];
+  avatar: Scalars['String'];
+  password?: Maybe<Scalars['String']>;
+  lang: Scalars['String'];
+  isDeleted: Scalars['Boolean'];
+  rooms: Array<Room>;
+  messages: Array<Maybe<Message>>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  translation: TranslationResponse;
+  createMessage: Scalars['Boolean'];
+  createRoom: CreateRoomResponse;
+  enterRoom: EnterRoomResponse;
+  deleteUser: Scalars['Boolean'];
+};
+
+export type MutationTranslationArgs = {
+  text: Scalars['String'];
+  target: Scalars['String'];
+};
+
+export type MutationCreateMessageArgs = {
+  text: Scalars['String'];
+};
+
+export type MutationCreateRoomArgs = {
+  nickname: Scalars['String'];
+  avatar: Scalars['String'];
+  lang: Scalars['String'];
+};
+
+export type MutationEnterRoomArgs = {
+  nickname: Scalars['String'];
+  avatar: Scalars['String'];
+  lang: Scalars['String'];
+  code: Scalars['String'];
+};
+
+export type TranslationResponse = {
+  __typename?: 'translationResponse';
+  translatedText: Scalars['String'];
+};
+
+export type CreateRoomResponse = {
+  __typename?: 'createRoomResponse';
+  userId: Scalars['Int'];
+  roomId: Scalars['Int'];
+  code: Scalars['String'];
+  token: Scalars['String'];
+};
+
+export type EnterRoomResponse = {
+  __typename?: 'enterRoomResponse';
+  userId: Scalars['Int'];
+  roomId: Scalars['Int'];
+  token: Scalars['String'];
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  newMessage?: Maybe<Message>;
+  deleteUser?: Maybe<DeleteUserResponse>;
+  newUser?: Maybe<User>;
+};
+
+export type SubscriptionNewMessageArgs = {
+  roomId: Scalars['Int'];
+  lang: Scalars['String'];
+};
+
+export type SubscriptionDeleteUserArgs = {
+  roomId: Scalars['Int'];
+};
+
+export type SubscriptionNewUserArgs = {
+  roomId: Scalars['Int'];
+};
+
+export type DeleteUserResponse = {
+  __typename?: 'deleteUserResponse';
+  id: Scalars['Int'];
+  roomId: Scalars['Int'];
+};
