8da7f5565f5a888d83c99527259b7ed4c19e62e2
ngocban <daongocban1102@gmail.com>
2022-09-21 22:09:37 +0700

    message queries (#6)

--- client/src/queries/message.queries.ts
+++ client/src/queries/message.queries.ts
@@ -1,1 +1,50 @@
 import { gql } from '@apollo/client';
+
+export const NEW_MESSAGE = gql`
+  subscription newMessage {
+    newMessage {
+      id
+      text
+      source
+      createdAt
+      user {
+        id
+        nickname
+        avatar
+        lang
+      }
+    }
+  }
+`;
+
+export const CREATE_MESSAGE = gql`
+  mutation createMessage($text: String!) {
+    createMessage(text: $text)
+  }
+`;
+
+export const ALL_MESSAGE_BY_PAGE = gql`
+  query allMessageByPage($page: Int!) {
+    allMessageByPage(page: $page) {
+      messages {
+        id
+        text
+        source
+        createdAt
+        user {
+          id
+          nickname
+          avatar
+          lang
+        }
+      }
+      nextPage
+    }
+  }
+`;
+
+export const CREATE_SYSTEM_MESSAGE = gql`
+  mutation createSystemMessage($source: String!) {
+    createSystemMessage(source: $source)
+  }
+`;
