9e60624bf549c25ce8156716d3a0e15aea5ac0f3
ngocban <daongocban1102@gmail.com>
2022-09-22 21:38:46 +0700

    change theme (#7)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import Avatar from '@components/UserProfile/Avatar';
+import React, { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 import { UserContextProvider } from './contexts/UserContext';
+import GlobalStyle from './styles/GlobalStyle';
+import { darkTheme, lightTheme } from './styles/Themes';
+import { Sun, Moon } from './components/Common/Icon';
 import Routes from './routes';
 
+const ThemeButton = styled.button`
+  position: fixed;
+  right: 2.5rem;
+  bottom: 2rem;
+  z-index: 1;
+  svg {
+    fill: ${({ theme }) => theme.text};
+  }
+  @media (max-width: ${({ theme }) => theme.mediaSize}) {
+    right: 1rem;
+    bottom: 0.5rem;
+    svg {
+      width: 25px;
+      height: 25px;
+    }
+  }
+`;
+
 const App: React.FC = () => {
+  const [theme, setTheme] = useState(false);
+  const toggleTheme = () => {
+    setTheme(!theme);
+  };
   return (
-    <UserContextProvider>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </UserContextProvider>
+    <ThemeProvider theme={theme ? darkTheme : lightTheme}>
+      <UserContextProvider>
+        <GlobalStyle />
+        <ThemeButton onClick={toggleTheme}>
+          {theme ? <Sun size={36} /> : <Moon size={36} />}
+        </ThemeButton>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </UserContextProvider>
+    </ThemeProvider>
   );
 };
 
